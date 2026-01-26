14e61a11c4b022db52b76e1d34f4ba63b6417e1e
ndolidze <n.dolidze@noxtton.com>
2026-01-26 11:21:20 +0400

    Add auto lock tooltip

--- src/pages/SettingsView/SettingsAdvancedTab/SettingsAutoLockConfiguration/styles.ts
+++ src/pages/SettingsView/SettingsAdvancedTab/SettingsAutoLockConfiguration/styles.ts
@@ -6,3 +6,29 @@ export const Wrapper = styled.div`
   gap: 10px;
   `
   
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  `
+
+  export const TooltipContent = styled.div`
+    font-family: 'Inter';
+    font-size: 14px;
+    line-height: 18px;
+    color: ${({ theme }) => theme.colors.white.mode1};
+  
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 400px;
+  `
+
+export const ListContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding-top: 5px;
+  padding-left: 20px;
+  list-style-type: disc;
+`
